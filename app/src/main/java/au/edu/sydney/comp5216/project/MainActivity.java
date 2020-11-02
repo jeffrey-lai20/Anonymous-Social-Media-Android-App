9a7a0d1a4f3b6b7bc04379382e97ef1bdbde54a8
Myx <maoyexin@outlook.com>
2020-11-02 17:17:28 +0800

    some UI with bugs

--- app/src/main/java/au/edu/sydney/comp5216/project/MainActivity.java
+++ app/src/main/java/au/edu/sydney/comp5216/project/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        startService(new Intent(this,MessageService.class));
+//        startService(new Intent(this,MessageService.class));
         navView.postDelayed(new Runnable() {
             @Override
             public void run() {
